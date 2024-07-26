dfc3ff9e1c2cbf659542df5122e2812cb75707b7
PandasCafe <colby.w.martin28@gmail.com>
2024-07-26 00:11:36 -0400

    Adjusted spice pouring mech, added temporary percentage thing

--- Pirate_GameJam/Assets/Colby/Scripts/SpiceSpawner.cs
+++ Pirate_GameJam/Assets/Colby/Scripts/SpiceSpawner.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SpiceSpawner : MonoBehaviour
 {
-    private int spiceAmount = 0;
+    private float spiceAmount = 0f;
     private ParticleSystem spiceParticleSystem;
     private float spicePourPercent;
     [SerializeField] private GameObject spiceHolder;
@@ -13,12 +14,19 @@ public class SpiceSpawner : MonoBehaviour
     [SerializeField] private float minParticles = 0f;
     [SerializeField] private float maxParticles = 0f;
     [SerializeField] private float moveSpiceSpeed = 10f;
+    [SerializeField] private float spicePourSpeed = .01f;
     [SerializeField] private bool pouring;
+    [SerializeField] private bool stillPouring;
     [SerializeField] private bool inPosition = false;
+    [SerializeField] private TMP_Text spicePercentText;
+    public float temporarySpiceGoal;
 
     private void Start()
     {
+        temporarySpiceGoal = 2000f;
+        stillPouring = false;
         pouring = false;
+        spicePercentText = GameObject.Find("SpicePercentText").GetComponent<TMP_Text>();
         pourSpicePos = GameObject.Find("PourSpicePosition");
         spiceHolder = GameObject.Find("SpiceParticleHolder");
         spiceParticleSystem = GetComponent<ParticleSystem>();
@@ -32,11 +40,21 @@ public class SpiceSpawner : MonoBehaviour
         {
             MoveSpiceWithMouse();
         }
-        else if (pouring != false)
+        else if (pouring == true)
         {
             MoveSpiceToPour();
         }
-
+        UpdateSpicePercent();
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        //Necessary for checking if the spice hits something
+        spiceAmount++;
+        //Debug.Log(spiceAmount);
+    }
+    private void PourSpice()
+    {
+        //Checks if the player is holding down left click, if they are then the spice particles spawn with a max of 250
         if (spiceHolder.transform.position.x < (pourSpicePos.transform.position.x + .2f) && spiceHolder.transform.position.x > (pourSpicePos.transform.position.x - .2f) &&
             spiceHolder.transform.position.y < (pourSpicePos.transform.position.y + .2f) && spiceHolder.transform.position.y > (pourSpicePos.transform.position.y - .2f))
         {
@@ -46,34 +64,39 @@ public class SpiceSpawner : MonoBehaviour
         {
             inPosition = false;
         }
-    }
-    void OnParticleCollision(GameObject other)
-    {
-        //Necessary for checking if the spice hits something
-        spiceAmount++;
-        Debug.Log(spiceAmount);
-    }
-    private void PourSpice()
-    {
-        //Checks if the player is holding down left click, if they are then the spice particles spawn with a max of 250
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+
+        if (Input.GetKeyDown(KeyCode.Mouse1) && !pouring)
         {
             pouring = true;
         }
         else if (Input.GetKeyUp(KeyCode.Mouse1))
         {
-            pouring = false;
-            minParticles = 0f;
+            stillPouring = true;
+            maxParticles = 45f;
+        }
+
+        if (inPosition && pouring && !stillPouring)
+        {
+            maxParticles = Mathf.Lerp(maxParticles, 1000f, ((Mathf.Lerp(0f, 50f, (.5f * Time.deltaTime))) * Time.deltaTime));
+            emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(minParticles, maxParticles);
+        }
+
+        if (stillPouring && maxParticles>0f)
+        {
+            maxParticles = Mathf.Lerp(maxParticles, -10f, (1f * Time.deltaTime));
+            emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(minParticles, maxParticles);
+        }
+
+        if (maxParticles<1 && stillPouring)
+        {
             maxParticles = 0f;
             emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(minParticles, maxParticles);
         }
-        if (inPosition && pouring)
+
+        if (stillPouring && maxParticles == 0f)
         {
-            while (maxParticles < 250f)
-            {
-                maxParticles = maxParticles + 1f;
-                emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(minParticles, maxParticles);
-            }
+            pouring = false;
+            stillPouring = false;
         }
     }
     private void MoveSpiceToPour()
@@ -87,6 +110,13 @@ public class SpiceSpawner : MonoBehaviour
         mousePos = Input.mousePosition;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
 
-        spiceHolder.transform.position = new Vector3(mousePos.x, mousePos.y, 0.0f);
+        spiceHolder.transform.position = new Vector3(Mathf.Lerp(spiceHolder.transform.position.x, mousePos.x, (10f * Time.deltaTime)), Mathf.Lerp(spiceHolder.transform.position.y, mousePos.y, (5f * Time.deltaTime)), 0.0f);
+    }
+
+    private void UpdateSpicePercent()
+    {
+        spicePourPercent = Mathf.FloorToInt(100 * (spiceAmount / temporarySpiceGoal));
+        spicePercentText.text = spicePourPercent + "%";
+        Debug.Log(spiceAmount);
     }
 }
